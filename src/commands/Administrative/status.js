539dcefac2527a5756ae0c31765bec73390fdc68
OGNova <jamooney2000@gmail.com>
2018-11-08 13:39:09 -0500

    Status command.

---
+++ src/commands/Administrative/status.js
@@ -0,0 +1,15 @@
+const Command = require('../../lib/structures/Command');
+
+class Status extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'status',
+      permLevel: 'Bot Admin'
+    });
+  }
+
+  async run(message, args, level) {
+    await this.client.user.setPresence({ activity: { name: `${args.join(' ')}` } });
+    message.reply(`I've set my status to \`${args.join(' ')}\`.`);
+  }
+}
