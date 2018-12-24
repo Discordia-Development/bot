25142a405b29c6949d145cfea3a7592f4ae0f03f
OGNova <jamooney2000@gmail.com>
2018-12-24 00:58:13 -0500

    Add master locale file.

---
+++ src/commands/restart.js
@@ -0,0 +1,31 @@
+const Command = require('../lib/structures/Command.js');
+const { promisify } = require('util');
+const exec = promisify(require('child_process').exec);
+
+
+class Reboot extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'reboot',
+      description: 'Restart the bot.',
+      category: 'System',
+      usage: 'reboot',
+      aliases: [],
+      permLevel: 'Bot Admin'
+    });
+  }
+
+  async run(message, args, level) { 
+    try {
+      await message.reply('Bot is shutting down.');
+      this.client.commands.forEach(async cmd => {
+        await this.client.unloadCommand(cmd);
+      });
+      process.exit(1);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+}
+
+module.exports = Reboot;
