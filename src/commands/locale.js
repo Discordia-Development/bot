eae0dec8fb043661106a78559023d28b4d87ff3a
OGNova <jamooney2000@gmail.com>
2018-12-25 16:40:09 -0500

    Was dumb, fixed dumb now.

--- src/commands/locale.js
+++ src/commands/locale.js
@@ -1,4 +1,5 @@
 const Command = require('../lib/structures/Command');
+const fs = require('fs-nextra');
 
 class Locale extends Command {
   constructor(client) {
@@ -14,11 +15,18 @@ class Locale extends Command {
     const key = 'locale';
 
     if (args.length < 1) {
+      const locales = await fs.readJSON(`${process.cwd()}/src/lib/languages/master.json`);
+      console.log(locales);
+      
+      for (const locale in locales) {
+        console.log(locale[longcode]);
+      }
+
       return message.reply(`your locale is currently set to \`${userSettings.locale}\`.`);
     } else {
       userSettings[key] = args.join(' ');
       await this.client.userSettings.set(message.author.id, userSettings);
-  
+
       return message.reply(`I've set your locale to \`${args.join(' ')}\`.`);
     }
   }
