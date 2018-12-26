b28e0c0af0e2017216bf21ba9f1631526088c001
OGNova <jamooney2000@gmail.com>
2018-12-25 20:32:01 -0500

    Thanks again, Tiemen.

--- src/commands/locale.js
+++ src/commands/locale.js
@@ -16,16 +16,21 @@ class Locale extends Command {
     if (args.length < 1) {
       return message.reply(`your locale is currently set to \`${userSettings.locale}\`.`);
     } else {
+      const arg = args.join(' ');
       const { locales } = require('../lib/languages/master.json');
       
-      const validLocale = locales.find(lang => lang.longcode === args.join(' '));
+      const validLocale = locales.find(lang => {
+        for (const val in lang)
+          if (lang[val].toLowerCase() === arg.toLowerCase())
+            return lang;
+      });
 
-      if (!validLocale) return message.reply(`The locale \`${args.join(' ')}\` is not a valid locale. Please check your spelling and try again.`);
+      if (!validLocale) return message.reply(`The locale \`${arg}\` is not a valid locale. Please check your spelling and try again.`);
 
-      userSettings[key] = args.join(' ');
+      userSettings[key] = validLocale.longcode;
       await this.client.userSettings.set(message.author.id, userSettings);
 
-      return message.reply(`I've set your locale to \`${args.join(' ')}\`.`);
+      return message.reply(`I've set your locale to \`${arg}\`.`);
     }
   }
 }
