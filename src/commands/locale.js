86b14751a910e2f9240cb4bcb373d37b84b18d3d
OGNova <jamooney2000@gmail.com>
2018-12-25 20:06:45 -0500

    Fix bigger dumb, thanks Tiemen.

--- src/commands/locale.js
+++ src/commands/locale.js
@@ -15,15 +15,14 @@ class Locale extends Command {
     const key = 'locale';
 
     if (args.length < 1) {
-      const locales = await fs.readJSON(`${process.cwd()}/src/lib/languages/master.json`);
-      console.log(locales);
-      
-      for (const locale in locales) {
-        console.log(locale[longcode]);
-      }
-
       return message.reply(`your locale is currently set to \`${userSettings.locale}\`.`);
     } else {
+      const { locales } = require('../lib/languages/master.json');
+      
+      const validLocale = locales.find(lang => lang.longcode === args.join(' '));
+
+      if (!validLocale) return message.reply(`The locale \`${args.join(' ')}\` is not a valid locale. Please check your spelling and try again.`);
+
       userSettings[key] = args.join(' ');
       await this.client.userSettings.set(message.author.id, userSettings);
 
