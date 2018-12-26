dd9168fbc0b7860b21808ee40af971f72ad8f8dd
OGNova <jamooney2000@gmail.com>
2018-12-26 00:04:31 -0500

    Stuff is hard.

--- src/commands/reload.js
+++ src/commands/reload.js
@@ -12,7 +12,7 @@ class Reload extends Command {
   }
 
   async run(message, args, level) { // eslint-disable-line no-unused-vars
-    if (!args.size) return message.reply('Must provide a command to reload. Derp.');
+    if (!args[0]) return message.reply('Must provide a command to reload. Derp.');
 
     const command = this.client.commands.get(args[0]) || this.client.commands.get(this.client.aliases.get(args[0]));
     if (!command) return message.reply(`The command \`${args[0]}\` does not exist, nor is it an alias.`);
