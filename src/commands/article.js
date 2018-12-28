4c17b9e6579b852cd4513aadc54cd455cff0d2a7
Tiemen <Tiemen2@live.nl>
2018-12-28 23:41:28 +0100

    ur undefined!

--- src/commands/article.js
+++ src/commands/article.js
@@ -33,8 +33,8 @@ class Article extends Command {
         const articles = await popularArticles();
         pageList = articles.map(async a => {
           const path = a.path.slice(1);
-          const { name } = await this.client.articleManager.load(client, path);
-          return `[${name}](https://discordia.me/${path})`;
+          const { name } = await this.client.articleManager.load(this.client, path);
+          return `[${await name}](https://discordia.me/${path})`;
         }).join('\n');
         this.client.redis.setex('popularArticles', 86400, pageList);
       } else {
@@ -98,7 +98,7 @@ class Article extends Command {
       }
 
       const url = `https://discordia.me/${search}`;
-      const { name } = await this.client.articleManager.load(client, search);
+      const { name } = await this.client.articleManager.load(this.client, search);
 
       return message.buildEmbed()
         .setColor(0x4A90E2)
